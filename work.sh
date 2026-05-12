1df4b8a55536d62e67164efa765bdb6fabf4aa5f
yyl-20020115 <yyl_20050115@hotmail.com>
2026-05-12 20:34:23 +0800

    changes work.sh

--- work.sh
+++ work.sh
@@ -2,5 +2,5 @@ cp client.py ./mineru/cli/client.py
 cp models_download_utils.py ./mineru/utils/models_download_utils.py
 cp Image.py ./.venv/lib/python3.13/site-packages/PIL/Image.py
 source .venv/bin/activate
-uv run mineru -p ~/Working/BIDDINGS -o ~/Working/BIDDINGS 
+uv run mineru -p ~/Working/BIDDINGS -o ~/Working/BIDDINGS_OUTPUT 
 
